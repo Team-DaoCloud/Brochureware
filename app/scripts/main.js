be1da0da1afc0d633bed19c0ea20f7404a59e4d2
Andrej Novikov <jazzaiman@gmail.com>
2015-08-31 09:57:12 +0300

    signup page

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -7,4 +7,20 @@
     $('.btn-cta-kerning').lettering();
     $('.btn-getstarted-cta-kerning').lettering();
   });
+    $('#carousel-signup').carousel({
+      interval: false
+    });
+    $('#signup-form').submit(function(e){
+      $('#carousel-signup').carousel(2);
+
+      $.ajax(s)
+      $.post('http://DaoCloud.us10.list-manage.com/subscribe/post',
+             $('#signup-form').serialize(), function(res){
+               console.dir(res);
+               $('#carousel-signup').carousel(2);
+             })
+      return false;
+    });
+    $('.signup-bg').height($(document).height() - 300);
+    $('.item').height($(document).height() - 400);
 })(jQuery);
