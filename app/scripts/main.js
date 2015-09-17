541a3eb3fe702d1ead0e6dac0899ecde71d14cfd
jserrao <bigbadserrao@gmail.com>
2015-09-17 02:52:52 -0400

    Added Andrej's cookie tracker to all the pages

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,64 +1,66 @@
 'use strict';
 /* global jQuery */
 
-// Little stuff
+// Sets up lettering.js span targets
 (function($) {
-  $(document).ready(function() {
-
-    // Sets up lettering.js span targets
-    $('.dao-banner-text-intro').lettering();
-    $('.btn-cta-kerning').lettering();
-    $('.btn-getstarted-cta-kerning').lettering();
-    $('.btn-different-cta-kerning').lettering();
-
-    // Gives us the current year
-    var currentTime = new Date();
-    var currentYear = currentTime.getFullYear();
-    $('.current-year').append(currentYear);
-  });
-
+  $('.dao-banner-text-intro').lettering();
+  $('.btn-cta-kerning').lettering();
+  $('.btn-getstarted-cta-kerning').lettering();
+  $('.btn-different-cta-kerning').lettering();
 })(jQuery);
 
-// Function to get mailchimp sign-up working for /signup page
-(function($){
-   $('#carousel-signup').carousel({
-     interval: false
-   });
+// Gives us the current year
+(function($) {
+  var currentTime = new Date();
+  var currentYear = currentTime.getFullYear();
+  $('.current-year').append(currentYear);
+})(jQuery);
 
-   $('#signup-form').submit(function(e){
-     $.ajax({
-       url: 'http://DaoCloud.us10.list-manage.com/subscribe/post-json?u=69306ec03132e1e4026684c81&id=66656777a9&c=?',
-       type: 'GET',
-       data: $('#signup-form').serialize(),
-       dataType: 'json',
-       contentType: 'application/json; charset=utf-8',
-       success: function (res){
-         console.dir(res);
-         if(res.result !== 'error'){
-           $('#carousel-signup').carousel(2);
-         }
-         else {
-           if(res.msg) {
-             if(res.msg[0] === '0') {
-               $('#form-email').addClass('has-error');
-               $('#form-errors').text(res.msg.substring(4));
-             }
-             else {
-               $('#signup-form').addClass('has-error');
-               $('#form-errors').text(res.msg);
-             }
-           }
-         }
-       }
-     });
-     return false;
-   });
+// Switches visibility on subhead text for 4-up explore section on homepage
+(function($) {
+  $(".dao-explore-unit-1").hover(function() {
+    $(".dao-explore-unit-1-content").fadeIn("linear");
+  });
+  $(".dao-explore-unit-2").hover(function() {
+    $(".dao-explore-unit-2-content").fadeIn("linear");
+  });
+  $(".dao-explore-unit-3").hover(function() {
+    $(".dao-explore-unit-3-content").fadeIn("linear");
+  });
+  $(".dao-explore-unit-4").hover(function() {
+    $(".dao-explore-unit-4-content").fadeIn("linear");
+  });
+})(jQuery); 
 
-   $('#signup-form input').keypress(function(){
-     $('#signup-form').removeClass('has-error');
-     $('#form-email').removeClass('has-error');
-   });
-   $('.item').css('min-height', function(){
-     return $(document).height() - 400;
-   });
+// This function sets a cookie for InfusionSoft Affiliate tracking that lasts 30 days
+function setCookie(cname, cvalue, exdays) {
+  var d = new Date();
+  d.setTime(d.getTime() + (exdays*24*60*60*1000));
+  var expires = "expires="+d.toUTCString()+';';
+  var path = 'path=/';
+  document.cookie = cname + "=" + cvalue + "; " + expires + path;
+}
+
+var getUrlParameter = function getUrlParameter(sParam) {
+  var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+    sURLVariables = sPageURL.split('&'),
+    sParameterName,
+    i;
+
+  for (i = 0; i < sURLVariables.length; i++) {
+    sParameterName = sURLVariables[i].split('=');
+
+    if (sParameterName[0] === sParam) {
+      return sParameterName[1] === undefined ? true : sParameterName[1];
+    }
+  }
+};
+
+// This function actually sets the cookie itself
+(function($) {
+  $(document).ready(function(){
+    var infusionAff = getUrlParameter('p');
+    if(infusionAff) setCookie('infusion_referrer', infusionAff, 30);
+    if(location.hash.slice(1).length > 0) setCookie('dao_ref', location.hash.slice(1), 1);
+  })
 })(jQuery);
