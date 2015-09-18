e61cffae100d8b2c93bb00273d96f9ed89a3c459
jserrao <bigbadserrao@gmail.com>
2015-09-18 11:58:40 -0400

    Revised deployment, double set of source files - gitignore was screwy

--- dist/scripts/partials.js
+++
@@ -1,6 +0,0 @@
-Handlebars.registerPartial("footer", Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
-    return "footer.hbs\n\nWhat the what?\n";
-},"useData":true}));
-Handlebars.registerPartial("header", Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
-    return "// This is the file header.hbs - checking to see if this actually comes through.\n\nvar navItems = {\n  \"navItem1\": {\n    \"url\": \"professionals\",\n    \"text\": \"Find Professionals\"\n  },\n  \"navitem2\": {\n    \"url\": \"../signin\",\n    \"text\": \"Sign In\"\n  }\n};\n\nvar headerTemplateScript = $(#header).html();\n";
-},"useData":true}));
