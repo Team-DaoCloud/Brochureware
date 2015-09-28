a5ac523fe48a961686902b1ed2a3c91b32fb63f7
jserrao <bigbadserrao@gmail.com>
2015-09-28 13:14:16 -0400

    Updated modal opening to happen in page and avoid minification

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -6,16 +6,6 @@
   $('.btn-cta-kerning').lettering();
   $('.btn-getstarted-cta-kerning').lettering();
   $('.btn-different-cta-kerning').lettering();
-
-  // Opens the watch video container on DaoCloud's homepage
-  $('#toggle-video').click(function(){
-    $('#watch-video').modal('toggle');
-  });
-
-  // Opens the schedule demo modal on Daocloud's pro page
-  $('#toggle-demo').click(function(){
-    $('#schedule-demo').modal('toggle');
-  });
 })(jQuery);
 
 // Gives us the current year
